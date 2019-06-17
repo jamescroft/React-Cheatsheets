18ce4fce8a0704e79589c82a376adb1e9f6ae7be
James Croft <james.croft@gmail.com>
2019-06-17 16:06:47 +1000

    adding a second item

--- js/main.js
+++ js/main.js
@@ -5,19 +5,16 @@ var clipboard = new ClipboardJS('.btn-clipboard');
 $(function () {
     $(".rcs-clipboard").tooltip({
         placement: "top",
-        title: "Copy to clipboard",
+        title: "Copy to clipboard"
     });
 })
 
 clipboard.on('success', function(e) {
     console.log(e);
     e.clearSelection();
-    $(".rcs-clipboard").attr('data-original-title', "Copied!").tooltip('show');
+    $('.rcs-clipboard').tooltip('dispose').tooltip({title: 'Copied!'}).tooltip('show');
 });
 
-$(".rcs-clipboard").mouseout(function(){
-    $(".rcs-clipboard").attr('data-original-title', "Copy to clipboard").tooltip('hide');
-})
 
 clipboard.on('error', function(e) {
     console.log(e);
