54f018cd704160e2a931a11c7b377afd9b8b5cf9
Croft <CroftJ@staff.tafe>
2019-06-18 10:34:52 +1000

    fixed tooltips, updated basic example

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
+// Tooltips
 
+$('.btn-clipboard').tooltip({
+    trigger: 'hover',
+    placement: 'top',
+    title: 'Copy to clipboard'
+  });
+  
+  function setTooltip(message) {
+    $(event.target).tooltip('hide')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
+  
+  function hideTooltip() {
+    setTimeout(function() {
+      $('.btn-clipboard').tooltip('hide')
+      .attr('data-original-title', 'Copy to clipboard');
+    }, 1000);
+  }
 
-var clipboard = new ClipboardJS('.btn-clipboard');
+  // Clipboard
 
-$(function () {
-    $(".rcs-clipboard").tooltip({
-        placement: "top",
-        title: "Copy to clipboard"
-    });
-})
-
-clipboard.on('success', function(e) {
-    console.log(e);
+  var clipboard = new ClipboardJS('.btn-clipboard');
+  
+  clipboard.on('success', function(e) {
+    setTooltip('Copied!');
+    hideTooltip();
     e.clearSelection();
-    $('.rcs-clipboard').tooltip('dispose').tooltip({title: 'Copied!'}).tooltip('show');
-});
-
-
-clipboard.on('error', function(e) {
-    console.log(e);
-});
+  });
+  
+  clipboard.on('error', function(e) {
+    setTooltip('Failed!');
+    hideTooltip();
+  });
+  
